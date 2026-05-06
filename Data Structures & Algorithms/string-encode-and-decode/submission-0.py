125d328d98ffe043f8507b9f2b628450afb515f0
Sy-Tuyen Ho (Tuyen) <32817741+hosytuyen@users.noreply.github.com>
2026-05-05 20:03:19 -0400

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_s = ""
+        for string in strs:
+            encoded_s += string + "~"
+        return encoded_s
+
+    def decode(self, s: str) -> List[str]:
+        return s.split("~")[:-1]
