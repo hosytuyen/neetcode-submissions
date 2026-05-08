9194bfb02487c055c79d888390ab47b45a7037d6
Sy-Tuyen Ho (Tuyen) <32817741+hosytuyen@users.noreply.github.com>
2026-05-08 12:19:47 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,30 @@
+class Solution:
+    def valid_character(self, c):
+        if ord('a') <= ord(c) <= ord('z'):
+            return True
+        if ord('A') <= ord(c) <= ord('Z'):
+            return True
+        if ord('0') <= ord(c) <= ord('9'):
+            return True
+        return False
+
+    def isPalindrome(self, s: str) -> bool:
+        left_index = 0
+        right_index = len(s) - 1
+        s = s.lower()
+
+        while left_index < right_index:
+            while not self.valid_character(s[left_index]):
+                left_index += 1
+                if left_index >= len(s):
+                    return True
+            while not self.valid_character(s[right_index]):
+                right_index -= 1
+
+            if s[left_index] == s[right_index]:
+                left_index += 1
+                right_index -= 1
+                continue
+            else:
+                return False
+        return True
