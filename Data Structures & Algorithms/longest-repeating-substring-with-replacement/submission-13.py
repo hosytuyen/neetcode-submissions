3f2a7fbd0b405f6e7bb956d67d4107316cc5afa6
Sy-Tuyen Ho (Tuyen) <32817741+hosytuyen@users.noreply.github.com>
2026-05-29 19:30:34 -0400

    Add: longest-repeating-substring-with-replacement - submission-13

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-13.py
@@ -0,0 +1,34 @@
+class Solution:
+    def count_freq(self, s):
+        freq = {}
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1
+        return freq
+    def characterReplacement(self, s: str, k: int) -> int:
+        max_len = 0
+        l = r = 0
+        while l < len(s):
+            freq = {}
+            max_freq = 0
+            last_max_freq = 0
+            r = l
+            while r < len(s):
+                freq[s[r]] = freq.get(s[r], 0) + 1
+                if max_freq < freq[s[r]]:
+                    max_freq = freq[s[r]]
+                    last_max_freq = r
+                replace_needed = r+1-l - max_freq
+                if replace_needed <= k:
+                    max_len = max(max_len, r+1-l)
+                else:
+                    i = last_max_freq
+                    break
+                r += 1
+            l += 1 
+        return max_len
+                
+
+                
+
+
+        
